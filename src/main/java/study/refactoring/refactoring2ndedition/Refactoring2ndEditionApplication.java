274aad356da8988714baf7e43ea278abed216cd0
lucia1 <lucia1@pulmuone.com>
2025-04-01 14:19:56 +0900

    Initial commit

---
+++ src/main/java/study/refactoring/refactoring2ndedition/Refactoring2ndEditionApplication.java
@@ -0,0 +1,13 @@
+package study.refactoring.refactoring2ndedition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Refactoring2ndEditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Refactoring2ndEditionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/refactoring/refactoring2ndedition/Refactoring2ndEditionApplicationTests.java
@@ -0,0 +1,13 @@
+package study.refactoring.refactoring2ndedition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Refactoring2ndEditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
