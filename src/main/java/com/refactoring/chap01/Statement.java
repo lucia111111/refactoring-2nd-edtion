343e3ced4076f53a32af84bd03557ca10c2d8238
lucia1 <lucia1@pulmuone.com>
2025-04-08 14:56:47 +0900

    chap01 함수 추출하기

--- src/main/java/com/refactoring/chap01/Statement.java
+++ src/main/java/com/refactoring/chap01/Statement.java
@@ -14,25 +14,7 @@ public class Statement {
 
         for (var perf : invoice.getPerformances()) {
             final Play play = plays.get(perf.getPlayId());
-            var thisAmount = 0;
-
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.getAudience() > 30) {
-                        thisAmount += 1000 * (perf.getAudience() - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-                    }
-                    thisAmount += 300 * perf.getAudience();
-                    break;
-                default:
-                    throw new IllegalArgumentException("알 수 없는 장르: " + play.getType());
-            }
+            var thisAmount = amountFor(perf, play);
 
             // 포인트를 적립한다.
             volumeCredits += Math.max(perf.getAudience() - 30, 0);
@@ -57,5 +39,29 @@ public class Statement {
         result.append(String.format("적립 포인트: %d점\n", volumeCredits));
 
         return result.toString();
+    }
+
+    // 값이 변하지 않는 변수는 매개변수로 전달
+    private int amountFor(Performance perf, Play play) {
+        int thisAmount;
+        switch (play.getType()) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.getAudience() > 30) {
+                    thisAmount += 1000 * (perf.getAudience() - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.getAudience() > 20) {
+                    thisAmount += 10000 + 500 * (perf.getAudience() - 20);
+                }
+                thisAmount += 300 * perf.getAudience();
+                break;
+            default:
+                throw new IllegalArgumentException("알 수 없는 장르: " + play.getType());
+        }
+        // 함수 안에서 값이 바뀌는 변수 반환
+        return thisAmount;
     }
 }
