f68a508a94723f6ede5ad67a9fbd28512bd51626
lucia1 <lucia1@pulmuone.com>
2025-04-11 15:45:48 +0900

    chap01 다형성을 활용하여 데이터 생성하기 ~76p

--- src/main/java/com/refactoring/chap01/ComedyCalculator.java
+++ src/main/java/com/refactoring/chap01/ComedyCalculator.java
@@ -4,4 +4,23 @@ public class ComedyCalculator extends PerformanceCalculator {
     public ComedyCalculator(Performance performance, Play play) {
         super(performance, play);
     }
+
+    @Override
+    public int amountFor() {
+        int thisAmount = 30000;
+        if (performance.getAudience() > 20) {
+            thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+        }
+        thisAmount += 300 * performance.getAudience();
+        return thisAmount;
+    }
+
+    @Override
+    public int volumeCreditsFor() {
+        // 포인트를 적립한다.
+        int result = Math.max(performance.getAudience() - 30, 0);
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        result += Math.floor(performance.getAudience() / 5);
+        return result;
+    }
 }
--- src/main/java/com/refactoring/chap01/TragedyCalculator.java
+++ src/main/java/com/refactoring/chap01/TragedyCalculator.java
@@ -4,4 +4,18 @@ public class TragedyCalculator extends PerformanceCalculator {
     public TragedyCalculator(Performance performance, Play play) {
         super(performance, play);
     }
+
+    @Override
+    public int amountFor() {
+        int thisAmount = 40000;
+        if (performance.getAudience() > 30) {
+            thisAmount += 1000 * (performance.getAudience() - 30);
+        }
+        return thisAmount;
+    }
+
+    @Override
+    public int volumeCreditsFor() {
+        return Math.max(performance.getAudience() - 30, 0);
+    }
 }
