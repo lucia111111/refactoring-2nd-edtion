25f84bc2c69e137879c96decb5617aeeb40356dd
lucia1 <lucia1@pulmuone.com>
2025-04-18 12:38:15 +0900

    chap06-3 변수추출하기 ~177p

--- src/main/java/com/refactoring/chap06/chap06_3/After.java
+++ src/main/java/com/refactoring/chap06/chap06_3/After.java
@@ -2,8 +2,10 @@ package com.refactoring.chap06.chap06_3;
 
 public class After {
     public double price(Order order) {
-        return order.quantity * order.itemPrice -
-                Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-                Math.min(order.quantity * order.itemPrice * 0.1 , 100);
+        double basePrice = order.quantity * order.itemPrice;
+        double quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+        double shipping = Math.min(basePrice * 0.1 , 100);
+                // 가격(price) = 기본 가격 - 수량 할인 + 배송비
+        return  basePrice - quantityDiscount + shipping;
     }
 }
