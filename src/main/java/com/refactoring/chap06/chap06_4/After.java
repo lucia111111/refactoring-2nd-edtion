1edb83e4e624a21222156d321951eef7dd95a4b7
lucia1 <lucia1@pulmuone.com>
2025-04-18 12:41:48 +0900

    chap06-4 변수 인라인하기 ~178p

--- src/main/java/com/refactoring/chap06/chap06_4/After.java
+++ src/main/java/com/refactoring/chap06/chap06_4/After.java
@@ -3,7 +3,6 @@ package com.refactoring.chap06.chap06_4;
 public class After {
 
     public boolean method(Order order) {
-        int basePrice = order.basePrice;
-        return basePrice > 1000;
+        return order.basePrice > 1000;
     }
 }
