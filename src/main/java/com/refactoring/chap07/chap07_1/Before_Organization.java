c5a528f5e1a015ccbdee2d6340cebaa38f3b01ef
lucia1 <lucia1@pulmuone.com>
2025-04-18 15:02:43 +0900

    chap07-1 레코드 캡슐화하기 before객체 생성 ~238p

---
+++ src/main/java/com/refactoring/chap07/chap07_1/Before_Organization.java
@@ -0,0 +1,13 @@
+package com.refactoring.chap07.chap07_1;
+
+/**
+ * 캡슐화 전 -> 단순 POJO 객체
+ *
+ * 어디서든 필드값 직접 변경 가능
+ * 캡슐화가 안 돼 있어서 객체의 일관성, 무결성 보장이 안 됨
+ *
+ * */
+public class Before_Organization {
+    public String name;
+    public String country;
+}
