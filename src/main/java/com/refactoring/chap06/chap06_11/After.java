c05b6c95b9891333f299d0bae0dfc98efce6eb4d
lucia1 <lucia1@pulmuone.com>
2025-04-18 14:35:36 +0900

    chap06-11 PriceData로 데이터 이동 ~220p

--- src/main/java/com/refactoring/chap06/chap06_11/After.java
+++ src/main/java/com/refactoring/chap06/chap06_11/After.java
@@ -5,16 +5,13 @@ public class After {
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
         int basePrice = product.basePrice * quantity;
         int discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-
-        PriceData priceData = new PriceData();
-        int price = applyShipping(priceData, quantity, shippingMethod, basePrice, discount);
-        return price;
+        PriceData priceData = new PriceData(quantity, discount, basePrice);
+        return applyShipping(priceData, shippingMethod);
     }
 
-    private int applyShipping(PriceData priceData, int quantity, ShippingMethod shippingMethod, int basePrice, int discount) {
-        int shippingPerCase = basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee : shippingMethod.feePerCase;
-        int shippingCost = quantity * shippingPerCase;
-        int price = basePrice - discount * shippingCost;
-        return price;
+    private int applyShipping(PriceData priceData, ShippingMethod shippingMethod) {
+        int shippingPerCase = priceData.getBasePrice() > shippingMethod.discountThreshold ? shippingMethod.discountFee : shippingMethod.feePerCase;
+        int shippingCost = priceData.getQuantity() * shippingPerCase;
+        return priceData.getBasePrice() - priceData.getDiscount() * shippingCost;
     }
 }
--- src/main/java/com/refactoring/chap06/chap06_11/PriceData.java
+++ src/main/java/com/refactoring/chap06/chap06_11/PriceData.java
@@ -4,4 +4,22 @@ public class PriceData {
     protected int basePrice;
     protected int quantity;
     protected int discount;
+
+    public PriceData(int quantity, int discount, int basePrice) {
+        this.quantity = quantity;
+        this.discount = discount;
+        this.basePrice = basePrice;
+    }
+
+    public int getBasePrice() {
+        return basePrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
 }
