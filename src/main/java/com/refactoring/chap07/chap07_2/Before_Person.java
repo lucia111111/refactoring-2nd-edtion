75351d6d554a507673f89540ff96513aae74d474
lucia1 <lucia1@pulmuone.com>
2025-04-22 08:33:26 +0900

    chap07-1,2 클래스 주석 추가

--- src/main/java/com/refactoring/chap07/chap07_2/Before_Person.java
+++ src/main/java/com/refactoring/chap07/chap07_2/Before_Person.java
@@ -3,6 +3,10 @@ package com.refactoring.chap07.chap07_2;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 가변 데이터를 캡슐화 → 데이터 구조가 언제 어떻게 수정되었는지 파악 용이
+ * 컬렉션을 감싼 클래스에 흔히 add()와 remove() 라는 컬렉션 변경자 메서드를 생성 → 컬렉션을 소유한 클래스를 통해서만 원소를 변경할 수 있도록
+ * */
 public class Before_Person {
     protected String name; 
     protected List<Before_Course> courses = new ArrayList<>();
